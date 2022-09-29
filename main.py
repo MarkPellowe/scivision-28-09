f4ca4eb82df1fca5a3c0534b3f9db096b7cdd56e
MarkPellowe <mjdpellowe@gmail.com>
2022-09-29 15:55:32 +0100

    Rubbish model is finished

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ models_catalog = default_catalog.models.to_dataframe()
 model_repo = models_catalog[models_catalog.name == "image-classifiers"].url.item()
 model_repo 
 model_repo = models_catalog[models_catalog.name == "image-classifiers"].url.item()
-cat_dog_classifier = load_pretrained_model(model_repo,model="catdogclassifier")
+cat_dog_classifier = load_pretrained_model(model_repo,model="catdogclassifier",)
 
